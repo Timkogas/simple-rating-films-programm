f99ae2d20e01891d066d041aa83e2a24b3ac0e2c
Timur Ermolaev <timkogas@gmail.com>
2022-02-07 11:37:47 +0300

    Написал главную переменную, с которой будем работать

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log("hello!");
+const movies = [
+    {
+        'name': 'Interstellar',
+        'ratings': {
+            'John': 10,
+            'Jack': 3
+        }
+    },
+    {
+        'name': 'Avengers: Infinity War',
+        'ratings': {
+            'Jack': 9,
+            'Jane': 10
+        }
+    }
+];
 //# sourceMappingURL=main.js.map
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,23 @@
-console.log("hello!")
+interface Movie {
+  name: string
+  ratings: {
+    [key:string]: number
+  }
+}
+
+const movies: Movie[] = [
+{
+  'name': 'Interstellar',
+  'ratings': {
+    'John': 10,
+    'Jack': 3
+  }
+},
+{
+  'name': 'Avengers: Infinity War',
+  'ratings': {
+    'Jack': 9,
+    'Jane': 10
+  }
+}
+]
